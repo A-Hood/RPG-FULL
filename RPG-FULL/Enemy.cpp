99f9688583184d81c913672e214ae3a449ed199d
A-Hood <17hood.a@thestudio-liverpool.net>
2023-03-10 12:59:48 +0000

    Balance changes.

--- RPG-FULL/Enemy.cpp
+++ RPG-FULL/Enemy.cpp
@@ -8,27 +8,27 @@ int* EnemyProfile(int enemySpawnType)
 	// BANDIT ENEMY TYPE !
 	if (enemySpawnType == 1)
 	{
-		enemyProfile[0] = 10; // HEALTH
+		enemyProfile[0] = 15; // HEALTH
 		enemyProfile[1] = 5; // MIN DAMAGE
-		enemyProfile[2] = 5; // MAX DAMAGE
+		enemyProfile[2] = 8; // MAX DAMAGE
 		enemyProfile[3] = 1; // ENEMY TYPE
 	}
 
 	// MERCANARY ENEMY TYPE !
 	else if (enemySpawnType == 2)
 	{
-		enemyProfile[0] = 20; // HEALTH
+		enemyProfile[0] = 25; // HEALTH
 		enemyProfile[1] = 10; // MIN DAMAGE
-		enemyProfile[2] = 10; // MAX DAMAGE
+		enemyProfile[2] = 15; // MAX DAMAGE
 		enemyProfile[3] = 2; // ENEMY TYPE
 	}
 
 	// TROLL ENEMY TYPE !
 	else if (enemySpawnType == 3)
 	{
-		enemyProfile[0] = 30; // HEALTH
-		enemyProfile[1] = 10; // MIN DAMAGE
-		enemyProfile[2] = 20; // MAX DAMAGE
+		enemyProfile[0] = 40; // HEALTH
+		enemyProfile[1] = 15; // MIN DAMAGE
+		enemyProfile[2] = 25; // MAX DAMAGE
 		enemyProfile[3] = 3; // ENEMY TYPE
 	}
 
