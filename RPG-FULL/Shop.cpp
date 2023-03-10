8762de2314182a13581c5f8d01428196ac414250
A-Hood <17hood.a@thestudio-liverpool.net>
2023-03-10 21:09:01 +0000

    Added upgrades to shop

--- RPG-FULL/Shop.cpp
+++ RPG-FULL/Shop.cpp
@@ -7,32 +7,60 @@
 
 int* Shop(int* ptrPProfile)
 {
-	std::string shopItems[4] = { "Health Potions", "NULL", "NULL", "NULL" };
-	int shopAmount[4] = { 5, 0, 0, 0 };
+	std::string shopItems[4] = { "Health Potions", "Weapon Upgrade", "Armour Upgrade", "NULL" };
+	int shopAmount[4] = { 2, 0, 0, 0 };
+	int shopPrices[4] = { 40, 0, 0 , 0 };
 	std::string* ptrShopI;
 	int* ptrShopA;
+	int* ptrShopC;
 	std::string yesOrNo, confirm;
 	ptrShopI = shopItems;
 	ptrShopA = shopAmount;
+	ptrShopC = shopPrices;
 	bool valid = false;
 	bool completePurchasing = false;
 	int itemSelect;
 	int optionSelect;
+
+
+	if (ptrPProfile[10] == 1)
+	{
+		shopAmount[1] = 1;
+		shopPrices[1] = 150;
+	}
+	else if (ptrPProfile[10] == 2)
+	{
+		shopAmount[1] = 1;
+		shopPrices[1] = 250;
+	}
+
+
+	if (ptrPProfile[11] == 1)
+	{
+		shopAmount[2] = 1;
+		shopPrices[2] = 150;
+	}
+	else if (ptrPProfile[11] == 2)
+	{
+		shopAmount[2] = 1;
+		shopPrices[2] = 250;
+	}
+
 
 	std::cout << "\n\n";
 	Type("=$=$=$=$=$=$= SHOP =$=$=$=$=$=$=", 40);
 
 	std::cout << "\n\n";
 	
-	ShopOutput(ptrShopI, ptrShopA);
+	ShopOutput(ptrShopI, ptrShopA, shopPrices);
 
 	std::cout << "\n\n\n";
 
-	Type("Would you like to purchase something? ", 30);
+	Type("Would you like to purchase something? (Yes or No): ", 30);
 	std::cin >> yesOrNo;
 	
 	// MAKE FULL STRING LOWERCASE
-	Lowercase(yesOrNo);
+	yesOrNo = Lowercase(yesOrNo);
 
 	//  STAY OR LEAVE SCRIPT
 	while (valid == false)
@@ -48,11 +76,23 @@ int* Shop(int* ptrPProfile)
 				{
 					if (ptrShopA[0] > 0)
 					{
-						ptrShopA[0] = ptrShopA[0] - 1;
-						ptrPProfile[7] = ptrPProfile[7] + 1;
-						Type("\n\nPurchased Health Potion! You now have ", 30);
-						std::cout << ptrPProfile[7];
-						Type(" Health Potions!", 30);
+						if (ptrPProfile[9] >= shopPrices[0])
+						{
+							ptrShopA[0] = ptrShopA[0] - 1; // TAKE AWAY FROM SHOP AMOUNT
+							ptrPProfile[7] = ptrPProfile[7] + 1; // ADD HEALTH POTION
+							ptrPProfile[9] = ptrPProfile[9] - shopPrices[0]; // TAKE AWAY COST
+							Type("\n\nPurchased Health Potion! You now have ", 30);
+							std::cout << ptrPProfile[7];
+							Type(" Health Potions!\n\nYou now have ", 30);
+							std::cout << ptrPProfile[9];
+							Type(" gold left!", 30);
+						}
+						else if (ptrPProfile[9] < shopPrices[0])
+						{
+							Type("\n\nSorry! You cannot afford that item! You only have ", 30);
+							std::cout << ptrPProfile[9];
+							Type(" gold!", 30);
+						}
 					}
 					else if (ptrShopA[0] == 0)
 					{
@@ -63,13 +103,61 @@ int* Shop(int* ptrPProfile)
 				// ITEM 2 PURCHASE
 				else if (itemSelect == 2)
 				{
-					Type("\n\nSorry, we are out of stock on that item!", 40);
+					if (ptrShopA[1] > 0)
+					{
+						if (ptrPProfile[9] >= shopPrices[1])
+						{
+							ptrShopA[1] = ptrShopA[1] - 1; // TAKE AWAY FROM SHOP AMOUNT
+							ptrPProfile[10] = ptrPProfile[10] + 1; // ADD WEAPON UPGRADE
+							ptrPProfile[9] = ptrPProfile[9] - shopPrices[1]; // TAKE AWAY COST
+							Type("\n\nPurchased Weapon Upgrade! You now have ", 30);
+							std::cout << ptrPProfile[10];
+							Type(" Upgrade!\n\nYou now have ", 30);
+							std::cout << ptrPProfile[9];
+							Type(" gold left!", 30);
+
+						}
+						else if (ptrPProfile[9] < shopPrices[1])
+						{
+							Type("\n\nSorry! You cannot afford that item! You only have ", 30);
+							std::cout << ptrPProfile[9];
+							Type(" gold!", 30);
+						}
+					}
+					else if (ptrShopA[1] == 0)
+					{
+						Type("\n\n*Checks under desk*\nSorry! I'm out of stock! I will be restocking soon.", 30);
+					}
 				}
 
 				// ITEM 3 PURCHASE
 				else if (itemSelect == 3)
 				{
-					Type("\n\nSorry, we are out of stock on that item!", 40);
+					if (ptrShopA[2] > 0)
+					{
+						if (ptrPProfile[9] >= shopPrices[2])
+						{
+							ptrShopA[2] = ptrShopA[2] - 1; // TAKE AWAY FROM SHOP AMOUNT
+							ptrPProfile[11] = ptrPProfile[11] + 1; // ADD ARMOU UPGRADE
+							ptrPProfile[9] = ptrPProfile[9] - shopPrices[2]; // TAKE AWAY COST
+							Type("\n\nPurchased Armour Upgrade! You now have ", 30);
+							std::cout << ptrPProfile[10];
+							Type(" Upgrade!\n\nYou now have ", 30);
+							std::cout << ptrPProfile[9];
+							Type(" gold left!", 30);
+
+						}
+						else if (ptrPProfile[9] < shopPrices[2])
+						{
+							Type("\n\nSorry! You cannot afford that item! You only have ", 30);
+							std::cout << ptrPProfile[9];
+							Type(" gold!", 30);
+						}
+					}
+					else if (ptrShopA[2] == 0)
+					{
+						Type("\n\n*Checks under desk*\nSorry! I'm out of stock! I will be restocking soon.", 30);
+					}
 				}
 
 				// ITEM 4 PURCHASE
@@ -94,7 +182,7 @@ int* Shop(int* ptrPProfile)
 					if (optionSelect == 1) // DISPLAY SHOP AGAIN
 					{
 						Type("\n\n=$=$=$=$=$=$= SHOP =$=$=$=$=$=$=\n\n", 10);
-						ShopOutput(ptrShopI, ptrShopA);
+						ShopOutput(ptrShopI, ptrShopA, ptrShopC);
 						std::cout << "\n";
 						itemSelect = 0;
 					}
@@ -112,7 +200,7 @@ int* Shop(int* ptrPProfile)
 					}
 				}
 
-				Type("\n\nWhat else do you want to buy?: ", 30);
+				Type("\n\nWhat else do you want to buy? (Enter 0 for shop options!): ", 30);
 				std::cin >> itemSelect;
 			}
 
@@ -120,9 +208,9 @@ int* Shop(int* ptrPProfile)
 		}
 		else if (yesOrNo == "no") // IF ANSWER == NO
 		{
-			Type("\n\nAre you sure you want to exit the shop?: ", 30);
+			Type("\n\nAre you sure you want to exit the shop? (Yes or No): ", 30);
 			std::cin >> confirm;
-			Lowercase(confirm);
+			confirm = Lowercase(confirm);
 			if (confirm == "yes")
 			{
 				Type("\n\nExiting shop!", 30);
@@ -140,19 +228,21 @@ int* Shop(int* ptrPProfile)
 			Type("\n=!=!= ERROR = !=!=\n\nChoice invalid! ", 40);
 		}
 		
-		Type("Re-enter your correct choice: ", 40);
+		Type("Re-enter your correct choice (Yes or No): ", 40);
 		std::cin >> yesOrNo;
 	}
 
 	return 0;
 }
 
-void ShopOutput(std::string* ptrShopI, int* ptrShopA)
+void ShopOutput(std::string* ptrShopI, int* ptrShopA, int* ptrShopC)
 {
 	// ITEM 1
 	Type("#1: ", 50);
 	std::cout << ptrShopI[0];
-	Type(" |   Amount: ", 50);
+	Type(" | Cost: ", 50);
+	std::cout << ptrShopC[0];
+	Type(" | Amount: ", 50);
 	std::cout << ptrShopA[0];
 	// ITEM 1 END
 
@@ -161,7 +251,9 @@ void ShopOutput(std::string* ptrShopI, int* ptrShopA)
 	//ITEM 2
 	Type("#2: ", 50);
 	std::cout << ptrShopI[1];
-	Type(" |   Amount: ", 50);
+	Type(" | Cost: ", 50);
+	std::cout << ptrShopC[1];
+	Type(" | Amount: ", 50);
 	std::cout << ptrShopA[1];
 	// ITEM 2 END
 
@@ -170,7 +262,9 @@ void ShopOutput(std::string* ptrShopI, int* ptrShopA)
 	//ITEM 3
 	Type("#3: ", 50);
 	std::cout << ptrShopI[2];
-	Type(" |   Amount: ", 50);
+	Type(" | Cost: ", 50);
+	std::cout << ptrShopC[2];
+	Type(" | Amount: ", 50);
 	std::cout << ptrShopA[2];
 	// ITEM 3 END
 
@@ -179,7 +273,9 @@ void ShopOutput(std::string* ptrShopI, int* ptrShopA)
 	//ITEM 4
 	Type("#4: ", 50);
 	std::cout << ptrShopI[3];
-	Type(" |   Amount: ", 50);
+	Type(" | Cost: ", 50);
+	std::cout << ptrShopC[3];
+	Type(" | Amount: ", 50);
 	std::cout << ptrShopA[3];
 	// ITEM 4 END
 }
