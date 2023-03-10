d538987c4de60d093a75f12884d869c3ea6f608e
A-Hood <17hood.a@thestudio-liverpool.net>
2023-03-10 21:06:43 +0000

    Enemy rewards

---
+++ RPG-FULL/GoldDrop.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
+#include "Header.h"
+
+int* EnemyRewards(int* ptrEProfile, int* ptrPProfile)
+{
+	int goldDrop;
+	goldDrop = rand() % (ptrEProfile[5] - ptrEProfile[4] - 1) + ptrEProfile[4];
+
+	ptrPProfile[9] = ptrPProfile[9] + goldDrop;
+
+	Type("\n\nThe enemy dropped ", 30);
+	std::cout << goldDrop;
+	Type(" gold! You now have ", 30);
+	std::cout << ptrPProfile[9];
+	Type(" gold!", 30);
+
+	return ptrPProfile;
+}
