be74a82a67334fb945822169b459f3e790615703
A-Hood <17hood.a@thestudio-liverpool.net>
2023-03-10 21:09:13 +0000

    Added weapon and armour upgrades

---
+++ RPG-FULL/Upgrades.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
+#include "Header.h"
+
+float* UpgradeCheck(int* ptrPProfile)
+{
+	static float upgradeArray[2];
+
+	// WEAPON NOT UPGRADED
+	if (ptrPProfile[10] == 1)
+	{
+		upgradeArray[0] = 1;
+	}
+
+	// WEAPON UPGRADE 1
+	else if (ptrPProfile[10] == 2)
+	{
+		upgradeArray[0] = 1.5;
+	}
+
+	// WEAPON UPGRADE 2
+	else if (ptrPProfile[10] == 3)
+	{
+		upgradeArray[0] = 1.8; 
+	}
+	
+	// ================================================ ARMOUR
+
+	// ARMOUR NOT UPGRADED
+	if (ptrPProfile[10] == 1)
+	{
+		upgradeArray[1] = 1;
+	}
+
+	// ARMOUR UPGRADE 1
+	else if (ptrPProfile[10] == 2)
+	{
+		upgradeArray[1] = 0.8;
+	}
+
+	// ARMOUR UPGRADE 2
+	else if (ptrPProfile[10] == 3)
+	{
+		upgradeArray[1] = 0.6; 
+	}
+
+	return upgradeArray;
+}
