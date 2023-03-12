876225ffbb6fdeeedba3d5af088ed2dad68e1a54
A-Hood <17hood.a@thestudio-liverpool.net>
2023-03-12 18:31:14 +0000

    Added boss gold rewards

--- RPG-FULL/GoldDrop.cpp
+++ RPG-FULL/GoldDrop.cpp
@@ -20,3 +20,19 @@ int* EnemyRewards(int* ptrEProfile, int* ptrPProfile)
 
 	return ptrPProfile;
 }
+
+int* BossRewards(int* ptrPProfile)
+{
+	int goldDrop;
+	goldDrop = rand() % (100 - 60 - 1) + 60;
+
+	ptrPProfile[9] = ptrPProfile[9] + goldDrop;
+
+	Type("\n\nThe boss dropped ", 30);
+	std::cout << goldDrop;
+	Type(" gold! You now have ", 30);
+	std::cout << ptrPProfile[9];
+	Type(" gold!", 30);
+
+	return ptrPProfile;
+}
